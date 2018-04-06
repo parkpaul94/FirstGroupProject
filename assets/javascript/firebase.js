39013d3d94d4ee9663681441ed72345ed0e8ce42
Paul Park <park.paul.94@gmail.com>
2018-04-06 13:59:08 -0400

    stylized login

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -12,45 +12,69 @@ firebase.auth().onAuthStateChanged(function(user) {
  if (user) {
      var user = firebase.auth().currentUser;
      if (user != null) {
-         var email_id = user.email;
+        var email_id = user.email;
+        var email_verified = user.emailVerified;
+        console.log(email_verified);
+        var welcomeuser = $('<div>').text('Welcome: ' + email_id);
+        console.log(email_id);
+        $('.welcomecontainer').show();
+        $('.welcomefont').html(welcomeuser);
+        $('.welcomecontainer').append($('.welcomefont'));
+        // var logout_button = $('<button>').text('Log Out').addClass('logoutbutton');
+        // $('.welcomecontainer').append(logout_button);
             //  $('.loginbutton').hide();
              $('.firebase-container').hide();
              $('.logoutbutton').show();
              $('#sdrinktitle').show();
-             $('.sDContent').show();
+             $('#drinktitle').show();
+             $('.sDContent').show();             
      }
  } else {
      $('.firebase-container').show();
+     $('.welcomecontainer').hide();
     //  $('.loginbutton').show();
+    // $('#drinktitle').hide();
      $('#sdrinktitle').hide();
      $('.sDContent').hide();
-     $('.logoutbutton').hide();
+    //  $('.logoutbutton').hide();
  }
  });
+function create() {
+    var userEmail = document.getElementById('email').value;
+    var userPW = document.getElementById('password').value;
+
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPW).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+   
+        alert('Error: ' + errorMessage);
+        // ...
+      });
+   }
 $('#password').on('keyup', function (event) {
     if (event.which === 13) {
         event.preventDefault();
-        searchText = $('#NSearched').val();
+        searchText = $('#email').val();
+        searchText = $('#password').val();
         login();
     }
 })
 $('.logoutbutton').on('click', function (event) {
-    var logoff = window.confirm('Do you want to log off?');
+    var logoff = window.confirm('Do you want to sign off?');
     if (logoff == true) {
         logout();
+        $('.welcomecontainer').hide();
+        $('.logoutbutton').hide();
     }
     else {
+        console.log('hello');
         return false;
     }
-    // if (event.which === 13) {
-    //     event.preventDefault();
-    //     searchText = $('#NSearched').val();
-    //     login();
-    // }
 })
 function login () {
- var userEmail = document.getElementById('email').value;
- var userPW = document.getElementById('password').value;
+    var userEmail = document.getElementById('email').value;
+    var userPW = document.getElementById('password').value;
  // $('.loginbutton').hide();
  // $('.main-container').hide();
  // $('#sdrinktitle').show();
